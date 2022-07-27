c9892477331e95a3431dfe3e3c28a626e8b42e8a
Carlos Garcia <carlosviniciogarcia1997@gmail.com>
2022-07-26 23:44:46 -0500

    first commit

---
+++ bot.js
@@ -0,0 +1,32 @@
+require('dotenv').config()
+const { Telegraf } = require('telegraf')
+const axios = require('axios')
+const { on } = require('nodemon')
+
+function random(number){
+    return Math.ceil(Math.random() * number)
+}
+const bot = new Telegraf(process.env.BOT_TOKEN)
+
+
+bot.start((ctx)=>{
+    ctx.reply("hola mundo")
+})
+
+bot.command('random', (ctx) =>{
+    
+    //ctx.reply(random(6))
+    ctx.replyWithDice()
+    //ctx.replyWithSticker('CAACAgEAAxkBAAOsX9WKywuspdVls5VSf9xV6ZLHrqAAAg8AA5390hUNDOUjryN26R4E')
+    
+} )
+
+bot.on('dice', async (ctx) => {
+    console.log(ctx.message)
+    await 
+    
+    
+    ctx.reply(`Value: ${ctx.message}`)
+})
+
+bot.launch();

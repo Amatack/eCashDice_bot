bf5bf22b52e9cfa2b6f944cfbd23cb976c60c5a8
Carlos Garcia <carlosviniciogarcia1997@gmail.com>
2022-07-26 23:57:15 -0500

    pequeño cambio

--- bot.js
+++ bot.js
@@ -1,20 +1,18 @@
 require('dotenv').config()
 const { Telegraf } = require('telegraf')
 const axios = require('axios')
-const { on } = require('nodemon')
+const {on} = require('nodemon')
 
 function random(number){
     return Math.ceil(Math.random() * number)
 }
 const bot = new Telegraf(process.env.BOT_TOKEN)
 
-
 bot.start((ctx)=>{
     ctx.reply("hola mundo")
 })
 
 bot.command('random', (ctx) =>{
-    
     //ctx.reply(random(6))
     ctx.replyWithDice()
     //ctx.replyWithSticker('CAACAgEAAxkBAAOsX9WKywuspdVls5VSf9xV6ZLHrqAAAg8AA5390hUNDOUjryN26R4E')

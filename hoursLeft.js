01f64fd766e78dc11b1bdb79f20e02f5da3d1acd
Carlos Garcia <carlosviniciogarcia1997@gmail.com>
2022-08-17 11:33:57 -0500

    Creamos la función hoursLeft.js

---
+++ hoursLeft.js
@@ -0,0 +1,17 @@
+
+export function hoursLeft(timeLeft){
+    let split = timeLeft.split(":")
+    split[0] = 23 - split[0]
+    split[1] = 60 - split[1]
+
+    if(split[1] === 60) {
+        split[1] = 0
+        split[0]++
+    }
+
+    if(split[0] < 10)  split[0] = "0" + split[0]
+    if(split[1] < 10)  split[1] = "0" + split[1]
+    
+    return split
+
+}

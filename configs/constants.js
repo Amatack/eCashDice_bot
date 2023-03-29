54afd5267a3a7d9a23a63943538eee942510af38
Carlos <carlosviniciogarcia1997@gmail.com>
2023-03-29 11:28:54 -0500

    update

--- configs/constants.js
+++ configs/constants.js
@@ -3,6 +3,9 @@ dotenv.config()
 
 export var timeoutId = new Array
 
-export const botAuth = process.env.BOT_TOKEN
+export const token = process.env.BOT_TOKEN
 export const idChat = process.env.ID_CHAT
+export const smtpPassword = process.env.SMTP
+export const emailAddress = process.env.EMAIL
+export const idChannel = process.env.ID_CHANNEL
 export const threadId = process.env.MESSAGE_THREAD_ID

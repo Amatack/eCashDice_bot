b90b54dc8b909e0254b765730c4b47ed975fb059
Carlos Garcia <carlosviniciogarcia1997@hotmail.com>
2024-10-28 15:14:04 -0500

    model created

---
+++ models/WinnerPool.js
@@ -0,0 +1,33 @@
+import {Schema, model} from "mongoose";
+
+const WinnerPoolScheme = new Schema(
+    {
+        name:{
+            type: String,
+            required:  false,
+            trim: true
+        },
+        teamName:{
+            type: String,
+            required:  false,
+            trim: true
+        },
+        totalPoolsBetWithoutVigorish: {
+            type: Number,
+            required:  true,
+            trim: true
+        },
+        withDiceService:{
+            type: Boolean,
+            required: false,
+            trim: true
+        }
+    },
+    {
+        collection: "winnerPool",
+        timestamps: true, //TODO createdAt, updatedAt
+        versionKey: false
+    }
+)
+
+export default model("WinnerPool", WinnerPoolScheme)
